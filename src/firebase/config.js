4acf75946d0eeae7782b629ccbf372ef11effea9
Ilham Suryoko <ilhamsuryoko11@gmail.com>
2020-09-30 06:21:44 +0700

    done

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAOWHBpPhKoNhcGFKHH_Q_0AtL2gV-imgQ',
-  authDomain: 'production-a9404.firebaseapp.com',
-  databaseURL: 'https://production-a9404.firebaseio.com',
-  projectId: 'production-a9404',
-  storageBucket: 'production-a9404.appspot.com',
-  messagingSenderId: '525472070731',
-  appId: '1:525472070731:web:ee873bd62c0deb7eba61ce',
+  apiKey: 'AIzaSyDgHJCusoRgEH76z8JR78vFXfoQm6_GMWE',
+  authDomain: 'test-survey-580aa.firebaseapp.com',
+  databaseURL: 'https://test-survey-580aa.firebaseio.com/',
+  projectId: 'test-survey-580aa',
+  storageBucket: 'test-survey-580aa.appspot.com',
+  messagingSenderId: '197013812693',
+  appId: '1:197013812693:web:405935fd82f4f4dd22f95c',
 };
 
 if (!firebase.apps.length) {
